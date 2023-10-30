13193d15c9e04984eb07466008f06a48db7384ed
Philipp Hess <he@boerse-group.de>
2023-10-30 12:21:23 +0100

    added showAll and findUser

---
+++ src/Controller/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class UserController extends AbstractController
+{
+    #[Route(path: '/showusers', name: 'showusers')]
+    public function show(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAllUsers();
+        return $this->render('user/show.html.twig', ['users' => $users]);
+    }
+
+    #[Route(path: '/finduser/{email}', name: 'finduser')]
+    public function findUser(UserRepository $userRepository, $email): Response
+    {
+        $user = $userRepository->findUserByEmail($email);
+    
+        return $this->render('user/find.html.twig', ['users' => [$user]]);
+    }
+}
